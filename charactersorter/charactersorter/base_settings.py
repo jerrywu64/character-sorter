e6b2a93435d687a733d23d353a63d5f431c8d085
Jerry Wu <werryju@mit.edu>
2018-07-18 15:21:19 -0700

    Split up settings file

--- charactersorter/charactersorter/base_settings.py
+++ charactersorter/charactersorter/base_settings.py
@@ -15,19 +15,6 @@ import os
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
-
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/2.0/howto/deployment/checklist/
-
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'uw-6#+^z)4s6sxzpqtz7+@rgj1)t2*q7^u0u=c@cjjt6x^8a%b'
-
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
-
-ALLOWED_HOSTS = []
-
-
 # Application definition
 
 INSTALLED_APPS = [
---
+++ charactersorter/charactersorter/local_settings.py
@@ -0,0 +1,12 @@
+from charactersorter.base_settings import *
+
+# Quick-start development settings - unsuitable for production
+# See https://docs.djangoproject.com/en/2.0/howto/deployment/checklist/
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = 'uw-6#+^z)4s6sxzpqtz7+@rgj1)t2*q7^u0u=c@cjjt6x^8a%b'
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+ALLOWED_HOSTS = []
