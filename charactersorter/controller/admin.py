4bdc601d31cc3c271de9775bc0268ea6c769995c
Jerry Wu <werryju@mit.edu>
2019-09-09 23:42:15 -0700

    Allow filtering by charlist in admin panel

--- charactersorter/controller/admin.py
+++ charactersorter/controller/admin.py
@@ -4,5 +4,6 @@ from .models import SortRecord
 
 class SortRecordAdmin(admin.ModelAdmin):
     readonly_fields = ('timestamp',)
+    list_filter = ('charlist',)
 
 admin.site.register(SortRecord, SortRecordAdmin)
